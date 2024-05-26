3889984b1ab12c42cb25f660e411a1cc93fde73c
blambertbrun <blambertbrun@etudiant.univ-rennes1.fr>
2024-05-26 02:47:45 +0200

    terminé fonctionnalité filtre projets

--- scrpit.js
+++ scrpit.js
@@ -1,31 +1,30 @@
-function defilementArticle()
-{
+/*-------------------------------- effets visuels globaux --------------------------------*/
+
+function defilementArticle(){       /*ajoute une légère animation lors de l'apprition d'un article*/
+    
     const observer = new IntersectionObserver((entries) => {
-        for(const entry of entries)
-            {
-                if(entry.isIntersecting)
-                    {
-                        entry.target.animate([
-                            {transform: 'translateY(100px)', opacity: 0},
-                            {transform: 'translateY(0px)', opacity: 1}
-                        ], {duration: 400})
-                    }
+        for(const entry of entries){
+            
+            if(entry.isIntersecting){
+            
+                entry.target.animate([
+                    {transform: 'translateY(100px)', opacity: 0},
+                    {transform: 'translateY(0px)', opacity: 1}
+                ], {duration: 400})
             }
+        }
     });
     
     
     const articles = document.querySelectorAll('article')
     
-    for(const a of articles)
-        {
-            observer.observe(a)
-        }
+    for(const article of articles){
+        observer.observe(article)
+    }
 }
 
-function apparitionNav()
-{
+function apparitionNav(){       /*les items de la navbar apparaissent au fur et à mesure au chargement de la page*/
     const nav_items = document.querySelectorAll(".nav_item")
-
 
     nav_items.forEach((item, index) => {
         setTimeout(() => {
@@ -40,107 +39,125 @@ function apparitionNav()
 }
 
 
-function apparitionTitre()
-{
+function apparitionTitre(){         /*ajoute une légère animation à l'apparition du titre au chargement de la page*/
     const header = document.querySelector("header")
     
     header.animate([
         {opacity: 0},
         {opacity: 1}
     ], {duration: 100})
+
     header.classList.add("visible")
 }
 
 
-function showProject(className, projectsMap)
-/*show every article belonging to the class |className| */
-{
-    let projects_to_show
 
-    if(className == "tous")
-        {
-            projects_to_show = Array.from(document.querySelectorAll("#projects-container article"))
-        }
-    else
-        {
-            projects_to_show = Array.from(document.getElementsByClassName(className))
-        }
+/*------------------------------- tri projets à afficher | page projets -------------------------------*/
+/* voir description de projectsMap dans checkboxProjects() pour mieux comprendre */
 
-    for(const project of projects_to_show)
-        {
-            projectsMap.set(project, projectsMap.get(project)+1)
 
-            if(projectsMap.get(project) == 1){
-                project.classList.add("visible_project")
-            }
-        }
-}
+function showProject(className, projectsMap){           /*gère l'affichage de tous les articles ayant la classe |className|*/
 
-function hideProject(className, projectsMap)
-{
-    let projects_to_hide
+    let projects_to_show /*array des projets à afficher*/
 
-    if(className == "tous")
-        {
-            projects_to_hide = Array.from(document.querySelectorAll("#projects-container article"))
-        }
-    else
-        {
-            projects_to_hide = Array.from(document.getElementsByClassName(className))
-        }
-
-    for(const project of projects_to_hide)
-        {
-            projectsMap.set(project, projectsMap.get(project)-1)
-
-            if(projectsMap.get(project) == 0){
-                project.classList.remove("visible_project")
-            }
+    if(className == "tous"){        /*décoche toutes les checkbox sauf "tous"*/
             
+        const checkboxs = Array.from(document.getElementsByTagName("input"))
+        checkboxs.splice(checkboxs.indexOf(document.getElementById('tous')), 1)
+
+        for(const c of checkboxs){
+            c.checked = false
         }
+
+        /*reset toutes les entrées de projectsMap*/
+        const projects = Array.from(document.querySelectorAll("#projects-container article"));
+        for(const project of projects){
+                projectsMap.set(project, 0)
+        }
+
+
+        projects_to_show = Array.from(document.querySelectorAll("#projects-container article"))
+    }
+
+    else{
+        if(document.querySelector("#tous").checked){        /*décoche la checkbox "tous" lorqu'une autre checkbox est cochée*/
+            
+            hideProject("tous", projectsMap)
+            document.querySelector("#tous").checked = false
+        }
+
+        projects_to_show = Array.from(document.getElementsByClassName(className))
+    }
+
+    for(const project of projects_to_show){
+        projectsMap.set(project, projectsMap.get(project)+1) /*met à jour projectsMap*/
+
+        if(projectsMap.get(project) == 1){
+            project.classList.add("visible_project")
+        }
+    }
 }
 
 
-function checkboxProjects()
-{
+
+function hideProject(className, projectsMap){       /*gère le masquage de tous les articles ayant la classe |className|*/
+
+    let projects_to_hide /*array des projets à masquer*/
+
+    if(className == "tous"){
+        projects_to_hide = Array.from(document.querySelectorAll("#projects-container article"))
+    }
+
+    else{
+        projects_to_hide = Array.from(document.getElementsByClassName(className))
+    }
+
+    for(const project of projects_to_hide){
+        
+        projectsMap.set(project, projectsMap.get(project)-1)
+
+        if(projectsMap.get(project) == 0){
+            project.classList.remove("visible_project")
+        }
+        
+    }
+}
+
+
+function checkboxProjects(){            /*fonction de gestion globale du tri des projets à afficher en fonction de checkboxs cochées*/
+
     const checkboxs = Array.from(document.getElementsByTagName("input"))
 
     const projects = Array.from(document.querySelectorAll("#projects-container article"));
 
-    let projectsMap = new Map() /*a map associating every article (project) with the amount of times a checkbox concerning it is checked
-                                (0 => should not be displyed, >0 => should be displayed)*/
+    let projectsMap = new Map()     /*une map qui associe cheque article (projet) avec le nombre de checkboxs cochées le concernant
+                                    (0 => ne doit pas être affiché, >0 => doit être affiché*/
 
 
-    for(const project of projects)
-        /*initiate every entry of the map*/
-        {
-            projectsMap.set(project, 0)
-        }
+    for(const project of projects){        /*initialise toutes les entrées de la map*/    
+        projectsMap.set(project, 0)
+    }
 
 
-    showProject("tous", projectsMap) /*when the page is loaded the option "tous" is checked by default*/
-    for(const c of checkboxs)
-        {
-            c.addEventListener("change", () => {
-                console.log(projectsMap)
-                if(c.checked)
-                    {
-                        showProject(c.id, projectsMap)
-                    }
-                else
-                {
-                    hideProject(c.id, projectsMap)
-                }
-            })
-        }
-    const projets = document.querySelectorAll('#projects-container article')
+    showProject("tous", projectsMap) /*car la checkbox "tous" est cochée par défaut au chargement de la page*/
+    for(const c of checkboxs){
+
+        c.addEventListener("change", () => {
+            console.log(projectsMap)
+            if(c.checked){
+                showProject(c.id, projectsMap)      /*l'id d'une checkbox porte le même nom que la classe sur laquelle elle est censée agir*/
+            }
+            else{
+                hideProject(c.id, projectsMap)
+            }
+        })
+    }
 }
 
 defilementArticle()
 apparitionNav()
 apparitionTitre()
 
-if(document.title == "projets BLB")
-{
+if(document.title == "projets BLB"){
     checkboxProjects()
 }
